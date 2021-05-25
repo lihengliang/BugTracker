b814092479693492c10480656c7fb073a7ea2ff6
Ethan Liang <liheng.liang@hotmail.com>
2021-05-25 22:00:06 +1000

    upgrade to bootstrap 5.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 import { LayoutModule } from './layout/layout.module';
@@ -22,7 +21,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    NgbModule,
     LayoutModule,
     AuthModule,
     FormsModule,
